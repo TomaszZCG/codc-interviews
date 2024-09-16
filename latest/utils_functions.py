d7bedec72230a939db17ab83b902b78626cf82f7
TomaszZCG <tomasz.zwarycz@capgemini.com>
2024-09-16 22:53:12 +0200

    del

--- latest/utils_functions.py
+++ latest/utils_functions.py
@@ -18,12 +18,3 @@ def write_csv_to_location(dataframe, location, filename):
         
     shutil.copy(os.path.join(filePathDestTemp, name), os.path.join(location, filename))            
     shutil.rmtree(filePathDestTemp)
-
-
-def filter_data(dataframe):
-    
-    filtered_df = dataframe.filter((col("country") == "United Kingdom") | (col("country") == "Netherlands"))
-    filtered_df = filtered_df.withColumnRenamed("id","client_identifier")\
-                         .withColumnRenamed("btc_a","bitcoin_address")\
-                         .withColumnRenamed("cc_t", "credit_card_type")
-    return filtered_df
