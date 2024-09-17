fbf43980a4b17805b7b213001cc4ad0a3334fdfb
TomaszZCG <tomasz.zwarycz@capgemini.com>
2024-09-17 15:52:11 +0200

    input params

--- src/version1/exercise_version1.py
+++ src/version1/exercise_version1.py
@@ -10,25 +10,38 @@ Background:
  the clients from the United Kingdom and the Netherlands and 
  some of their financial details to starting reaching out to 
  them for a new marketing push.
+
+ example run of application: tzwarycz@LCE64920:~/codc_interviews/src/version1$ /usr/bin/python3 /home/tzwarycz/codc_interviews/src/version1/exercise_version1.py file_name.csv 
+
 """
 
 from pyspark.sql import SparkSession
 from utils import write_csv_to_location, filter_data, read_dataset
+import sys
 
-output_folder = "client_data"  
-file_name = "dataset_three.csv"
-spark = SparkSession.builder.appName("JoinDatasets").getOrCreate()
 
-df1 = read_dataset(spark,'dataset_one.csv')
-df2 = read_dataset(spark,'dataset_two.csv')
+def main(file_name):
+    output_folder = "client_data"  
+    spark = SparkSession.builder.appName("JoinDatasets").getOrCreate()
 
-df_joined = df1.join(df2, on="id", how="inner")
+    df1 = read_dataset(spark,'dataset_one.csv')
+    df2 = read_dataset(spark,'dataset_two.csv')
 
-df_drop = df_joined.drop("cc_n")\
-                 .drop("first_name")\
-                 .drop("last_name")
+    df_joined = df1.join(df2, on="id", how="inner")
 
-df_filtered = filter_data(df_drop)
-df_filtered.show()
+    df_drop = df_joined.drop("cc_n")\
+                    .drop("first_name")\
+                    .drop("last_name")
 
-write_csv_to_location(df_filtered, output_folder, file_name)
+    df_filtered = filter_data(df_drop)
+    df_filtered.show()
+
+    write_csv_to_location(df_filtered, output_folder, file_name)
+
+
+
+if __name__ == "__main__":
+    """ file name """
+    file_name = sys.argv[1]
+    """ run the main function """
+    main(file_name)
